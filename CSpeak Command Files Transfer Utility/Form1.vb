b7b0bb591b59a573be31f55a46377bc2a431068f
Tim. <Tim-GitHub>
2014-07-15 14:33:47 -0700

    Comments Added
    Added comments for easier reference and understanding.

--- CSpeak Command Files Transfer Utility/Form1.vb
+++ CSpeak Command Files Transfer Utility/Form1.vb
@@ -17,16 +17,25 @@
     End Sub
 
     Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
-        Dim List2 As New List(Of String)
-        Dim index As Integer
-        List2.AddRange(CmdFiles.Text.Split({Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries))
-        For index = 0 To List2.Count - 1
+        'Main code for detecting matched files and copying data
+        'Defining variables
+        Dim CmdFileList As New List(Of String)
+        Dim i As Integer
+        'Converting multi-line text field listing command files generated using CSpeak Command Generator
+        CmdFileList.AddRange(CmdFiles.Text.Split({Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries))
+        'Looping through each new command file containing data to be copied to the server(s)
+        For i = 0 To CmdFileList.Count - 1
+            'Looping through each file in the server path(s) looking for a match to the new command filename
             For Each DestinationFile In System.IO.Directory.GetFiles(ServerPaths.Text)
-                If System.IO.Path.GetFileName(DestinationFile) = System.IO.Path.GetFileName(List2(index)) Then
-                    MsgBox(System.IO.Path.GetFileName(DestinationFile) & System.IO.Path.GetFileName(List2(index)) & " Matches!")
+                'When a match is found the process continues
+                If System.IO.Path.GetFileName(DestinationFile) = System.IO.Path.GetFileName(CmdFileList(i)) Then
+                    MsgBox(System.IO.Path.GetFileName(DestinationFile) & System.IO.Path.GetFileName(CmdFileList(i)) & " Matches!")
                 Else
+                    'When a match is not found then the next file from the server path(s) are checked
                 End If
             Next
+            'Looping through each file on the server is complete
         Next
+        'Looping through each command filename is complete
     End Sub
 End Class
